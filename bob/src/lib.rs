bbeb438753d5edbc04372af72c79c069947a2a14
Miguel Berrio <miguelberrio91@gmail.com>
2019-09-28 17:22:53 +0200

    Fix bob

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,34 +1,21 @@
 pub fn reply(message: &str) -> &str {
-    if message.trim() == "" {
+    let msg = message.trim();
+    if msg.is_empty() {
         return "Fine. Be that way!";
     }
 
-    let message_vec: Vec<char> = message.trim().chars().collect();
-    let last = message_vec.last();
-    if last == Some(&'?') {
-        if is_shouting(&message_vec) {
-            return "Calm down, I know what I'm doing!";
-        } else {
-            return "Sure.";
-        }
-    } else if is_shouting(&message_vec) {
+    let question = msg.chars().last() == Some('?');
+    let msg_chars: Vec<char> = msg.chars().filter(|c| c.is_ascii_alphabetic()).collect();
+    let yell = msg_chars.len() > 0 && msg_chars.iter().all(|c| c.is_ascii_uppercase());
+
+    if question && yell {
+        return "Calm down, I know what I'm doing!";
+    }
+    if question {
+        return "Sure.";
+    }
+    if yell {
         return "Whoa, chill out!";
     }
-    return "Whatever.";
-}
-
-fn is_shouting(msg: &Vec<char>) -> bool {
-    let letters = msg
-        .clone()
-        .into_iter()
-        .filter(|c| c.is_alphabetic())
-        .collect::<Vec<char>>();
-
-    !letters.is_empty()
-        && &letters
-            .into_iter()
-            .filter(|l| l.is_uppercase())
-            .collect::<Vec<char>>()
-            .len()
-            == &letters.len()
+    "Whatever."
 }
