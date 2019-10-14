69d2e6f13226fd1ecc8b81671115c5fcb7b4c6d3
Miguel Berrio <miguelberrio91@gmail.com>
2019-10-14 18:58:21 +0200

    clock: mentor's feedback

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct Clock {
     hours: i32,
     minutes: i32,
@@ -15,19 +15,11 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let carry = (self.minutes + minutes).div_euclid(60);
-        Clock {
-            hours: (self.hours + carry).rem_euclid(24),
-            minutes: (self.minutes + minutes).rem_euclid(60),
-        }
-    }
-
-    pub fn to_string(&self) -> String {
-        format!("{:?}", self)
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
-impl fmt::Debug for Clock {
+impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
