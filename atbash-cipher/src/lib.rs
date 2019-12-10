573d35e7c5d91b888a027f1c6abccad8cc38c169
Miguel Berrio <miguelberrio91@gmail.com>
2019-12-10 18:31:51 +0100

    atbash-cipher: Try with Iters

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -4,13 +4,8 @@ use std::char;
 pub fn encode(plain: &str) -> String {
     reverse_ascii_string(plain)
         .enumerate()
-        .fold(String::new(), |acc, (i, c)| {
-            if i != 0 && i % 5 == 0 {
-                format!("{} {}", acc, c)
-            } else {
-                format!("{}{}", acc, c)
-            }
-        })
+        .flat_map(|(i, c)| format(i, c))
+        .collect::<String>()
 }
 
 /// "Decipher" with the Atbash cipher.
@@ -18,11 +13,18 @@ pub fn decode(cipher: &str) -> String {
     reverse_ascii_string(cipher).collect::<String>()
 }
 
+fn format(i: usize, c: char) -> impl Iterator<Item = char> {
+    if i != 0 && i % 5 == 0 {
+        Some(' ').into_iter().chain(Some(c))
+    } else {
+        Some(c).into_iter().chain(None)
+    }
+}
+
 fn reverse_ascii_string<'a>(input: &'a str) -> impl Iterator<Item = char> + 'a {
     input.chars().filter(char::is_ascii_alphanumeric).map(|c| {
         if c.is_ascii_alphabetic() {
-            char::from_u32('z' as u32 - c.to_ascii_lowercase() as u32 + 'a' as u32)
-                .expect("Can't convert to char")
+            (b'z' - c.to_ascii_lowercase() as u8 + b'a') as char
         } else {
             c
         }
