9ae5bf44c22026c0637fd25dee504618f2f6b0bc
Miguel Berrio <miguelberrio91@gmail.com>
2020-08-14 23:01:07 +0200

    anagram: Modify code from feedback

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,19 +1,19 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
-    let mut sorted_word = word.to_lowercase().chars().collect::<Vec<char>>();
-    sorted_word.sort_by(|a, b| b.cmp(a));
+    let mut sorted_word: Vec<char> = word.to_lowercase().chars().collect();
+    sorted_word.sort();
     possible_anagrams
         .iter()
         .filter(|&a| {
-            if a.to_lowercase() == word.to_lowercase() || a.len() != word.len() {
-                return false;
+            if a.len() != word.len() || a.to_lowercase() == word.to_lowercase() {
+                false
+            } else {
+                let mut chars = a.to_lowercase().chars().collect::<Vec<char>>();
+                chars.sort();
+                chars.eq(&sorted_word)
             }
-
-            let mut chars = a.to_lowercase().chars().collect::<Vec<char>>();
-            chars.sort_by(|a, b| b.cmp(a));
-            chars.eq(&sorted_word)
         })
         .cloned()
-        .collect::<HashSet<&'a str>>()
+        .collect()
 }
